d43be91688bf17ff7cdfffdec64b08c7c91f2fca
Loui Recio <lreciopersonal@gmail.com>
2023-09-14 23:14:02 -0500

    stand by

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python im using Conda
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -20,3 +20,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
